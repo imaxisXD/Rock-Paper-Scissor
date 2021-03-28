31e155007eb0f23efc272ff5df40d34b7c2d53a2
ABHISHEK <56172425+imaxisXD@users.noreply.github.com>
2021-03-28 16:00:12 +0530

    completed V1

--- script.js
+++ script.js
@@ -1,14 +1,77 @@
 console.log("Script Loaded");
+let computerChoice = 0;
+let computerScore = 0;
+let playerScore = 0;
+let roundCount = 0;
+let playerChoice = 0;
+
 const choice = [
     "rock",
     "paper",
     "scissor"
 ];
 
-let computerPlay = () => {    
-    const randomChoice = choice[Random.nextInt(chocie.length)];
-    console.log(randomChoice);
-    return randomChoice;
+const computerPlay = () => {    
+    const computerTurn = choice[Math.floor( Math.random() * choice.length )];    
+    return computerTurn;
 }
 
-computerPlay();
+const playerPlay = () => {
+    let choice = window.prompt("Enter Rock or Paper or Scissor");
+    return choice;
+}
+
+const playRound = (computerChoice, playerChoice) => {
+
+    computerChoice = computerPlay().toLowerCase();
+    playerChoice = playerPlay().toLowerCase();
+    console.log(computerChoice, playerChoice);
+
+    if (computerChoice === playerChoice) {
+        console.log("It's a DRAW !");
+    }
+    //Winning Condition For Computer
+   else if (( computerChoice === 'rock' && playerChoice === 'scissor' )||
+    ( computerChoice === 'scissor' && playerChoice === 'paper' )||
+    (computerChoice === 'paper' && playerChoice === 'rock')) {
+        computerScore++;
+        roundCount++;
+        console.log(`Round Num : ${roundCount}, Computer : ${computerScore}, Player : ${playerScore}`);
+        if(roundCount == 5){
+            // call function game over
+            gameOver(playerScore,computerScore);
+        }    
+    }
+    //Winning Condition For Player  
+    else if (( playerChoice === 'rock' && computerChoice === 'scissor' )||
+    ( playerChoice === 'scissor' && computerChoice === 'paper' )||
+    (playerChoice === 'paper' && computerChoice === 'rock')) {
+        playerScore++;
+        roundCount++;
+        console.log(`Round Num : ${roundCount}, Computer : ${computerScore}, Player : ${playerScore}`);
+        if(roundCount == 5){
+            gameOver(playerScore,computerScore);
+        }
+    }
+
+    else {
+        console.log("Error in condition");
+    }
+}
+
+const gameOver = (playerScore, computerScore) => {
+    
+    if( playerScore > computerScore)
+        console.log("Congratulation !, You Won");
+    else if( playerScore < computerScore)
+        console.log("You Lost");
+    else    
+        console.log("It's a Draw")
+}
+
+const playGame = (noOfRounds) => {
+    while (noOfRounds>0) {
+        playRound()
+        noOfRounds-- ;
+    }
+}
